6a94c266a45ac8e6537da44124b9016cbda6b29e
GANIDU2413 <ganidusahan@gmail.com>
2024-02-26 08:58:44 +0530

    added

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,4 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 public class Student implements Serializable {
     public String stuName;
@@ -9,13 +7,25 @@ public class Student implements Serializable {
 
     public static void main(String[] args) {
         Student student = new Student();
-        student.stuName="Rashini";
+        student.stuName="Ganidu";
         student.stuAddress="Gampaha";
         student.stuID=706;
 
         try {
             FileOutputStream fo = new FileOutputStream("student.ser");
+            //Serializing the object
+            ObjectOutputStream objo = new ObjectOutputStream(fo);
+            objo.writeObject(student);
+
+            System.out.println("Student Serialized...");
+
+            objo.close();
+            fo.close();
+
+
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
